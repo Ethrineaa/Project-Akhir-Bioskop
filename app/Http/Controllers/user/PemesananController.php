d4c6d3ddc9d0a7238cd96eb03936311330ee92fc
Hafidz-tech <hafidzmagfiroh633@gmail.com>
2025-12-08 21:48:45 +0700

    controller smeneta

--- app/Http/Controllers/user/PemesananController.php
+++ app/Http/Controllers/user/PemesananController.php
@@ -3,10 +3,18 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Jadwal;
+use App\Models\Kursi;
 
 class PemesananController extends Controller
 {
-    //
+    public function pilihKursi($jadwal_id)
+    {
+        $jadwal = Jadwal::with('studio')->findOrFail($jadwal_id);
+
+        // Ambil status kursi berdasarkan jadwal
+        $kursi = Kursi::where('studio_id', $jadwal->studio_id)->get();
+
+        return view('user.kursi.index', compact('jadwal', 'kursi'));
+    }
 }
-w
