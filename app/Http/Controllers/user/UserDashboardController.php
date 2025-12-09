477040f09e3f7f8350c0994c0ed1a9d8d05bfc2f
Hafidz-tech <hafidzmagfiroh633@gmail.com>
2025-12-09 14:37:35 +0700

    sementara a

--- app/Http/Controllers/user/UserDashboardController.php
+++ app/Http/Controllers/user/UserDashboardController.php
@@ -9,8 +9,12 @@ class UserDashboardController extends Controller
 {
     public function index()
     {
+        // Ambil film terbaru
         $films = Film::latest()->get();
 
-        return view('user.dashboard', compact('films'));
+        // PAKAI WELCOME SEBAGAI DASHBOARD
+        return view('welcome', [
+            'films' => $films
+        ]);
     }
 }
