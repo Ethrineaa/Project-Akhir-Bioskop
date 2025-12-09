15a5409fa716909864885485c7671e278d8c83fd
Hafidz-tech <hafidzmagfiroh633@gmail.com>
2025-12-09 14:23:03 +0700

    modal pesan

--- app/Models/Pemesanan.php
+++ app/Models/Pemesanan.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pemesanan extends Model
 {
-    //
+    protected $fillable = [
+        'jadwal_id',
+        'user_id',
+        'jumlah_tiket',
+        'total_harga'
+    ];
+
+    public function jadwal()
+    {
+        return $this->belongsTo(Jadwal::class);
+    }
+
+    public function kursis()
+    {
+        return $this->belongsToMany(Kursi::class, 'kursi_pemesanan', 'pemesanan_id', 'kursi_id');
+    }
 }
