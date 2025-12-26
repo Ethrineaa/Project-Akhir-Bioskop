653843038431681f79e66206c5a7673d7a11d04c
Hafidz-tech <hafidzmagfiroh633@gmail.com>
2025-12-26 20:07:33 +0700

    sementara

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -3,8 +3,8 @@
     <div>
         <div class="brand-logo d-flex align-items-center justify-content-between">
             <a href="./index.html" class="text-nowrap logo-img">
-                <img src="{{ asset('template-admin/src/assets/images/logos/logo.png') }}" width="180"
-                    alt="Logo SIMINLAB" />
+                <img src="{{ asset('template-admin/src/assets/logo/cinema-removebg.png') }}" width="180"
+                    alt="Logo Cinema" />
             </a>
             <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
                 <i class="ti ti-x fs-8"></i>
