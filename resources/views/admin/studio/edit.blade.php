49258be95d39156ba220800287a8cbe23ef57f37
Hafidz-tech <hafidzmagfiroh633@gmail.com>
2026-02-03 12:11:24 +0700

    final

--- resources/views/admin/studio/edit.blade.php
+++ resources/views/admin/studio/edit.blade.php
@@ -1,24 +1,24 @@
 @extends('admin.layouts.app')
 
 @section('content')
-<div class="card">
-    <div class="card-body">
+    <div class="card">
+        <div class="card-body">
 
-        <h5 class="card-title mb-4">Edit Studio</h5>
+            <h5 class="card-title mb-4">Edit Studio</h5>
 
-        <form action="{{ route('admin.studio.update', $studio->id) }}" method="POST">
-            @csrf @method('PUT')
+            <form action="{{ route('admin.studio.update', $studio->id) }}" method="POST">
+                @csrf @method('PUT')
 
-            <div class="mb-3">
-                <label>Nama Studio</label>
-                <input type="text" name="nama" class="form-control" value="{{ $studio->nama }}" required>
-            </div>
+                <div class="mb-3">
+                    <label>Nama Studio</label>
+                    <input type="text" name="nama" class="form-control" value="{{ $studio->nama }}" required>
+                </div>
 
-            <button class="btn btn-primary">Update</button>
-            <a href="{{ route('admin.studio.index') }}" class="btn btn-secondary">Kembali</a>
+                <button class="btn btn-primary">Update</button>
+                <a href="{{ route('admin.studio.index') }}" class="btn btn-secondary">Kembali</a>
 
-        </form>
+            </form>
 
+        </div>
     </div>
-</div>
 @endsection
